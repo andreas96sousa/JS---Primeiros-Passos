afeffe1a067856137463ace56bae6595135e6cf8
Andreas Sousa <devandreas96@gmail.com>
2025-02-21 20:13:05 -0300

    .

--- 03/script.js
+++ 03/script.js
@@ -3,12 +3,22 @@ let resetar = document.querySelector("#resetar");
 let aumentar = document.querySelector("#aumentar");
 let contador = document.querySelector("#contador");
 
+
+
 diminuir.addEventListener("click", () => diminuirContador());
 resetar.addEventListener("click", () => resetarContador());
 aumentar.addEventListener("click", () => aumentarContador());
 
-function diminuirContador(a) {
-  let numero = Number(contador);
+function diminuirContador() {
+  let numero = Number(contador.textContent);
   let menos = numero - 1;
+  contador.innerText = menos;
+}
+function resetarContador(){
+  contador.innerText = "0"
+}
+function aumentarContador() {
+  let numero = Number(contador.textContent);
+  let menos = numero + 1;
   contador.innerText = menos;
 }
