98c830762905650db2fbf370fd3715fe30317f3c
Andreas Sousa <devandreas96@gmail.com>
2025-02-21 22:41:22 -0300

    exercicios

--- 03/script.js
+++ 03/script.js
@@ -1,24 +1,30 @@
-let diminuir = document.querySelector("#diminuir");
-let resetar = document.querySelector("#resetar");
-let aumentar = document.querySelector("#aumentar");
-let contador = document.querySelector("#contador");
+let diminuir = document.querySelector("#diminuir"); // atribui o botão de diminuir
+let resetar = document.querySelector("#resetar"); // atribui o botão de resetar
+let aumentar = document.querySelector("#aumentar"); // atribui o botão de aumentar
+let contador = document.querySelector("#contador"); // atribui o contador
 
 
 
-diminuir.addEventListener("click", () => diminuirContador());
-resetar.addEventListener("click", () => resetarContador());
-aumentar.addEventListener("click", () => aumentarContador());
+diminuir.addEventListener("click", () => diminuirContador()); // evento de click para o botão de diminuir
+resetar.addEventListener("click", () => resetarContador()); // evento de click para o botão de resetar
+aumentar.addEventListener("click", () => aumentarContador()); // evento de click para o botão de aumentar
 
+
+//função para diminuir
 function diminuirContador() {
-  let numero = Number(contador.textContent);
-  let menos = numero - 1;
-  contador.innerText = menos;
+  let numero = Number(contador.textContent); //transforma o texto escrito dentro do html em numero
+  let menos = numero - 1; // subtrai 1 do numero editado sempre que clica
+  contador.innerText = menos; // reescreve o html
 }
+
+//função para resetar
 function resetarContador(){
-  contador.innerText = "0"
+  contador.innerText = "0";  // reescreve o html para "0"
 }
+
+//função para aumentar
 function aumentarContador() {
-  let numero = Number(contador.textContent);
-  let menos = numero + 1;
-  contador.innerText = menos;
+  let numero = Number(contador.textContent); //transforma o texto escrito dentro do html em numero
+  let menos = numero + 1; // soma + 1 ao numero editado sempre que clica
+  contador.innerText = menos; // reescreve o html
 }
