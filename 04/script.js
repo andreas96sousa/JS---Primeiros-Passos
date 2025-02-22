6d4f1c0be5a453a1a67cc64014533e9a41df3e5a
andreas96sousa <devandreas96@gmail.com>
2025-02-22 09:59:41 -0300

    .

--- 04/script.js
+++ 04/script.js
@@ -1,37 +1,44 @@
-const anteriorbtn = document.querySelector("#anteriorbtn")
-const proximobtn = document.querySelector("#proximobtn")
+const anteriorbtn = document.querySelector("#anteriorbtn");
+const proximobtn = document.querySelector("#proximobtn");
+let nomeDoCliente = document.querySelector("#nomedocliente");
 
-proximobtn.addEventListener("click", () => proximoCliente())
-anteriorbtn.addEventListener("click", () => anteriorCliente())
+proximobtn.addEventListener("click", () => proximoCliente());
+anteriorbtn.addEventListener("click", () => anteriorCliente());
 
 const cliente01 = {
   nome: "Maria Madalena",
-  lorem: "Cliente 01 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?"
-}
+  lorem:
+    "Cliente 01 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?",
+};
 const cliente02 = {
   nome: "Outro Nome Qualquer",
-  lorem: "Cliente 02 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?"
-}
+  lorem:
+    "Cliente 02 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?",
+};
 const cliente03 = {
   nome: "Nayara Oliveira",
-  lorem: "Cliente 03 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?"
-}
+  lorem:
+    "Cliente 03 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?",
+};
 const cliente04 = {
   nome: "Andreas Sousa",
-  lorem: "Cliente 04 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?"
-}
+  lorem:
+    "Cliente 04 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?",
+};
 
-const clientes = [cliente01, cliente02, cliente03, cliente04]
+const clientes = [cliente01, cliente02, cliente03, cliente04];
 
 let numero = 1;
-function proximoCliente() { 
+nomeDoCliente.innerText = clientes[numero - 1].nome;
+function proximoCliente() {
   numero = numero + 1;
   if (numero > 4) {
     numero = 1; // Reinicia para 1 ao ultrapassar 4
+
+    nomeDoCliente.innerText = clientes[0].nome;
   }
   let imagem = document.querySelector("#imagemCliente");
   imagem.setAttribute("src", "media/cliente" + numero + ".jpg");
-  
 }
 
 function anteriorCliente() {
@@ -42,5 +49,3 @@ function anteriorCliente() {
   let imagem = document.querySelector("#imagemCliente");
   imagem.setAttribute("src", "media/cliente" + numero + ".jpg");
 }
-
-
--- tester.js
+++ tester.js
@@ -1,20 +1,24 @@
-const cliente01 = [
-    nome = "Maria Madalena",
-    lorem = "Cliente 01 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?"
-  ]
-  const cliente02 = [
-    nome = "Outro Nome Qualquer",
-    lorem = "Cliente 02 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?"
-  ]
-  const cliente03 = [
-    nome = "Nayara Oliveira",
-    lorem = "Cliente 03 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?"
-  ]
-  const cliente04 = [
-    nome = "Andreas Sousa",
-    lorem = "Cliente 04 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?"
-  ]
-const clientes = [cliente01, cliente02, cliente03, cliente04]
-let numero = clientes.length
-console.log(numero)
-console.log(clientes[0].nome);
+const cliente01 = {
+  nome: "Maria Madalena",
+  lorem:
+    "Cliente 01 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?",
+};
+const cliente02 = {
+  nome: "Outro Nome Qualquer",
+  lorem:
+    "Cliente 02 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?",
+};
+const cliente03 = {
+  nome: "Nayara Oliveira",
+  lorem:
+    "Cliente 03 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?",
+};
+const cliente04 = {
+  nome: "Andreas Sousa",
+  lorem:
+    "Cliente 04 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, amet quidem, harum minus praesentium nobis ipsa reiciendis totam, quam inventore soluta vitae. Totam, possimus labore neque est rerum eveniet sapiente?",
+};
+
+const clientes = [cliente01, cliente02, cliente03, cliente04];
+
+console.log(clientes[3].nome);
